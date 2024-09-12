6d028bb55316a9505871fe50a98afd8ca12f8fec
miraclety <mirandacvh27@gmail.com>
2024-09-12 14:49:12 +0700

    Praktikum 4D- Membuat Class dari Class Diagram

---
+++ empat.php
@@ -0,0 +1,85 @@
+<?php
+class Author {
+    public $name;
+    public $description;
+
+    public function __construct($name, $description) {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function show($type) {
+        if ($type == 'array') {
+            return [
+                'name' => $this->name,
+                'description' => $this->description
+            ];
+        }
+    }
+}
+Class Publisher {
+    public $name;
+    public $address;
+    public $phone;
+
+    public function __construct($name, $address, $phone) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->phone = $phone;
+    }
+
+    public function setPhone($phone) {
+        $this->phone = $phone;
+    }
+
+    public function getPhone() {
+        return $this->phone;
+    }
+}
+Class Book {
+    public $ISBN;
+    public $title;
+    public $description;
+    public $category;
+    public $language;
+    public $numberOfPage;
+    public $author;
+    public $publisher;
+
+    public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher) {
+        $this->ISBN = $ISBN;
+        $this->title = $title;
+        $this->description = $description;
+        $this->category = $category;
+        $this->language = $language;
+        $this->numberOfPage = $numberOfPage;
+        $this->author = $author;
+        $this->publisher = $publisher;
+    }
+
+    public function showAll() {
+        return [
+            'ISBN' => $this->ISBN,
+            'title' => $this->title,
+            'description' => $this->description,
+            'category' => $this->category,
+            'language' => $this->language,
+            'numberOfPage' => $this->numberOfPage,
+            'author' => $this->author->show('array'),
+            'publisher' => [
+                'name' => $this->publisher->name,
+                'address' => $this->publisher->address,
+                'phone' => $this->publisher->getPhone()
+            ]
+        ];
+    }
+
+    public function detail($ISBN) {
+        if ($this->ISBN == $ISBN) {
+            return $this->showAll();
+        } else {
+            return null;
+        }
+    }
+}
+?>
