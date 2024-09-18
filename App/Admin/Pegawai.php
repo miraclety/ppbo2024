df41b0bea8169d1c9d16d443db86c2767ea16d94
miraclety <mirandacvh27@gmail.com>
2024-09-18 20:55:57 +0700

    Praktikum 6E - Menggunakan Pewarisan

--- App/Admin/Pegawai.php
+++ App/Admin/Pegawai.php
@@ -1,4 +1,6 @@
 <?php
+namespace App\Admin;
+
 class Pegawai {
     public $nip;
     public $nama;
@@ -27,33 +29,6 @@ class Pegawai {
 
     public function setNoHp($no_hp) {
         $this->no_hp = $no_hp;
-
-    }
-}
-
-class Dosen extends Pegawai {
-    public $nidn;
-
-    public function __construct($nip, $nama, $no_hp, $alamat, $nidn) {
-        parent::__construct($nip, $nama, $no_hp, $alamat);
-        $this->nidn = $nidn;
-    }
-
-    public function mengajar() {
-        echo "{$this->nama} sedang mengajar perkuliahan";
-
-    }
-}
-
-class TenagaKependidikan extends Pegawai {
-    public $gaji_pokok;
-
-    public function __construct($nip, $nama, $no_hp, $alamat, $gaji_pokok) {
-        parent::__construct($nip, $nama, $no_hp, $alamat);
-        $this->gaji_pokok = $gaji_pokok;
-    }
-
-    public function cuti() {
 
     }
 }
---
+++ App/Admin/TenagaKependidikan.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Admin;
+
+class TenagaKependidikan extends Pegawai {
+    public $gaji_pokok;
+    private $sisaCuti = 12;
+
+    public function __construct($nip, $nama, $no_hp, $alamat, $gaji_pokok) {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->gaji_pokok = $gaji_pokok;
+    }
+
+    public function cuti($jumlahHari) {
+        if ($this->sisaCuti >= $jumlahHari) {
+            $this->sisaCuti -= $jumlahHari;
+            echo "{$this->nama} telah mengambil {$jumlahHari} hari cuti. Sisa cuti: {$this->sisaCuti} hari.";
+        } else {
+            echo "{$this->nama} tidak memiliki cukup sisa cuti. Sisa cuti hanya {$this->sisaCuti} hari.";
+        }
+    }
+
+    public function getSisaCuti() {
+        return $this->sisaCuti;
+    }
+}
